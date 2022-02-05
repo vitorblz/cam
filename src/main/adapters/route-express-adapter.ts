e46d592ff240876400a1046bd22ebc5a790b1183
Vitor Carvalho <vitor.carvalho@opinionbox.com>
2022-02-05 16:42:08 -0300

    feat: create routes abstractions

--- src/main/adapters/route-express-adapter.ts
+++ src/main/adapters/route-express-adapter.ts
@@ -4,8 +4,12 @@ import { HttpResponse } from "../ports/http";
 
 export function routeExpressAdapter(controller: Controller){
     return  async (req: Request, res: Response) =>{
-       const httpResponse: HttpResponse = await  controller.handler(req);
-       res.status(httpResponse.statusCode).json(httpResponse.body);
+        try {
+            const httpResponse: HttpResponse = await  controller.handler(req);
+            res.status(httpResponse.statusCode).json(httpResponse.body);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
     }
 }
 
